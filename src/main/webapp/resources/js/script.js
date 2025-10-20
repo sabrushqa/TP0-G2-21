033c585148848dd55c12a2f104a02716bfc3fefb
sabreeena <sabrenalakehal12@gmail.com>
2025-10-20 23:56:53 +0100

    Ajout de la page JSF, CSS, JS

---
+++ src/main/webapp/resources/js/script.js
@@ -0,0 +1,14 @@
+function copyToClipboard(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.select();
+        element.setSelectionRange(0, 99999); 
+
+        try {
+            document.execCommand('copy');
+            alert('Contenu copié dans le presse-papiers !');
+        } catch (err) {
+            alert('Erreur lors de la copie : ' + err);
+        }
+    }
+}
